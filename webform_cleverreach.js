372a488c746452fabc6b1d4d8f51942a05fd6c1d
Peter Philipp <peter.philipp@cando-image.com>
2016-06-13 19:43:00 +1200

    Integrated all new parts: * Added class autoloader / integrate with registry_autoload * Integrated with webforms - based on webform_mailchimp. * Startet using new API * Added wrapper function

---
+++ webform_cleverreach.js
@@ -0,0 +1,19 @@
+
+jQuery(document).ready(function($) {
+  $('#edit-extra-use-existing-email-field').change(function(){
+    var sel = $("#edit-extra-use-existing-email-field option:selected");
+    if (sel[0]["value"] != 'cleverreach_field') {
+      $('#field_settings').show();
+    }
+    else {
+      $('#field_settings').hide();
+    }
+  });
+
+  if ($("#edit-extra-use-existing-email-field option:selected")[0]['value'] != 'cleverreach_field') {
+    $('#field_settings').show();
+  }
+  else {
+    $('#field_settings').hide();
+  }
+});
