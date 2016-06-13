72eedb5b3b9b6f8e89c24b59e655f8a70249ffe8
derhasi <derhasi@83474.no-reply.drupal.org>
2016-06-14 11:53:24 +1200

    Issue #2135241 by derhasi, hctom: Implement double opt-in

---
+++ src/CleverReachForm.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\cleverreach;
+
+class CleverReachForm {
+
+  public $id;
+
+  public $name;
+
+  public $description;
+
+  public function __construct(array $data = array()) {
+    $this->setUp($data);
+  }
+
+  protected function setUp($data) {
+    foreach ($data as $key => $val) {
+      $this->{$key} = $val;
+    }
+  }
+}
